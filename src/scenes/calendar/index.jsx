ce5705de539238ef4332ce8d415bbdf003aa6c0c
Mass Jack <77391015+massjack1@users.noreply.github.com>
2023-02-16 21:29:15 +0100

    Fix import error

--- src/scenes/calendar/index.jsx
+++ src/scenes/calendar/index.jsx
@@ -1,5 +1,6 @@
 import {useState} from "react";
-import FullCalendar, { formatDate } from "@fullcalendar/core";
+import FullCalendar from "@fullcalendar/react";
+import { formatDate } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
@@ -87,45 +88,45 @@ import {
           </Box>
   
           {/* CALENDAR */}
-          <Box flex="1 1 100%" ml="15px">
-            <FullCalendar
-              height="75vh"
-              plugins={[
-                dayGridPlugin,
-                timeGridPlugin,
-                interactionPlugin,
-                listPlugin,
-              ]}
-              headerToolbar={{
-                left: "prev,next today",
-                center: "title",
-                right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
-              }}
-              initialView="dayGridMonth"
-              editable={true}
-              selectable={true}
-              selectMirror={true}
-              dayMaxEvents={true}
-              select={handleDateClick}
-              eventClick={handleEventClick}
-              eventsSet={(events) => setCurrentEvents(events)}
-              initialEvents={[
-                {
-                  id: "12315",
-                  title: "All-day event",
-                  date: "2022-09-14",
-                },
-                {
-                  id: "5123",
-                  title: "Timed event",
-                  date: "2022-09-28",
-                },
-              ]}
-            />
-          </Box>
+        <Box flex="1 1 100%" ml="15px">
+          <FullCalendar
+            height="75vh"
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+            ]}
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listMonth",
+            }}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            select={handleDateClick}
+            eventClick={handleEventClick}
+            eventsSet={(events) => setCurrentEvents(events)}
+            initialEvents={[
+              {
+                id: "12315",
+                title: "All-day event",
+                date: "2022-09-14",
+              },
+              {
+                id: "5123",
+                title: "Timed event",
+                date: "2022-09-28",
+              },
+            ]}
+          />
         </Box>
       </Box>
-    );
-  };
-  
-  export default Calendar;
+    </Box>
+  );
+};
+
+export default Calendar;
